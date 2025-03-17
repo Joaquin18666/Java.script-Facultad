8e079feba11d367f63c9299605906c3db2dc3a2a
Joaquin18666 <jllambiolivera@itba.edu.ar>
2025-03-17 19:36:32 -0300

    first commit

---
+++ clase2.js
@@ -0,0 +1,10 @@
+// ARRAYS SON LISTAS
+
+const NUMEROS = [1,2,3,4,5] // CUANDO ES UNA CONSTANTE POR CONVENCION SE ESCRIBE EN MAYUSCULA
+console.log (NUMERO5[0]) // print de python
+NUMEROS[0] = 'ESTEBAN'
+NUMEROS.push('HOLA') //APPEND
+console.log(NUMEROS)
+NUMEROS.pop()
+NUMEROS.shift()//remueve el primero
+NUMEROS.unshift('Javier')
