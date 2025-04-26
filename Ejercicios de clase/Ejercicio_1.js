89ede87a7dc65855daf2b0b67fdc6910496804c6
Joaquin18666 <jllambiolivera@itba.edu.ar>
2025-04-25 21:55:54 -0300

    commit

---
+++ Ejercicios de clase/Ejercicio_1.js
@@ -0,0 +1,30 @@
+let ciudad = 'Buenos aires';
+let pais = 'Argentina';
+let edad = '18/01/2005';
+console.log('ciudad:',ciudad)
+console.log('pais:',pais)
+console.log('edad:',edad)
+ciudad = 'Mendoza'
+pais = 'Chile'
+edad = '18/10/2005'
+console.log('nueva ciudad:',ciudad)
+console.log('nuevo pais:',pais)
+console.log('nueva edad:',edad)
+
+//let te permite cambiar el valor de la variable después de declararla.
+
+//const crea una variable constante, lo que significa que no puedes cambiar 
+// su valor después de declararla.
+
+
+
+// const ciudad = "Buenos Aires";
+//       const pais = "Argentina";
+//       const anioNacimiento = 2000;
+
+//       console.log("CONST - Original:");
+//       console.log("Ciudad:", ciudad);
+//       console.log("País:", pais);
+//       console.log("Año de nacimiento:", anioNacimiento);
+
+      // Intentar cambiar los valores (esto causará error)////////////////////////////////////////////
